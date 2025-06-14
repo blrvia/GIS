e1cabfdf2a34f39194d5fab411746f972046232f
blrvia <blrviakop@gmail.com>
2025-06-14 23:26:25 +0530

    Update landParcelRoutes.js

--- backend/routes/landParcelRoutes.js
+++ backend/routes/landParcelRoutes.js
@@ -1,1 +1,14 @@
+const express = require('express');
+const router = express.Router();
+const firebaseAuthMiddleware = require('../middleware/firebaseAuthMiddleware');
+const landParcelController = require('../controllers/landParcelController');
+
+router.use(firebaseAuthMiddleware);
+
+router.get('/', landParcelController.getParcels);
+router.post('/', landParcelController.addParcel);
+router.put('/:id', landParcelController.updateParcel);
+router.delete('/:id', landParcelController.deleteParcel);
+
+module.exports = router;
 
