06cc2268272456324fb92ffbfe6181fe79442f1b
blrvia <blrviakop@gmail.com>
2025-06-14 23:26:39 +0530

    Update app.js

--- backend/app.js
+++ backend/app.js
@@ -1,1 +1,18 @@
+const express = require('express');
+const cors = require('cors');
+const landParcelRoutes = require('./routes/landParcelRoutes');
+require('dotenv').config();
+
+const app = express();
+
+app.use(cors());
+app.use(express.json());
+
+app.use('/api/parcels', landParcelRoutes);
+
+app.get('/', (req, res) => {
+  res.send('Land Bank Backend Running');
+});
+
+module.exports = app;
 
